c2a23ac6157d4c2a225b6990475d048f36a619c8
delafildzania <42663150+delafildzania@users.noreply.github.com>
2018-10-29 21:51:38 +0700

    Add files via upload

--- modul8.sql
+++ modul8.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Okt 2018 pada 05.39
--- Versi server: 10.1.34-MariaDB
--- Versi PHP: 7.2.7
+-- Waktu pembuatan: 29 Okt 2018 pada 15.49
+-- Versi server: 10.1.35-MariaDB
+-- Versi PHP: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -38,15 +38,18 @@ CREATE TABLE `jurnal` (
   `tgl` date NOT NULL,
   `username` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `password` varchar(20) NOT NULL,
+  `foto` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `jurnal`
 --
 
-INSERT INTO `jurnal` (`nama`, `nim`, `kelas`, `hobi`, `genre`, `wisata`, `tgl`, `username`, `email`, `password`) VALUES
-(' ', '', ' ', ' ', '', '', '0000-00-00', 'devifildzania', 'devifildzania@gmail.com', 'devi1234567');
+INSERT INTO `jurnal` (`nama`, `nim`, `kelas`, `hobi`, `genre`, `wisata`, `tgl`, `username`, `email`, `password`, `foto`) VALUES
+(' M. Dava Fachrulah', '6701172222', 'd3mi45', ' makan', 'Action', 'Bali', '2018-10-29', 'davafachrulah', 'davafachrulah@gmail.com', 'dava123', 'icon.png'),
+(' Devi Fildzania', '6701171052', 'd3mi41', ' nobar', 'Drama', 'Lombok', '2018-10-27', 'devifildzania', 'delafildzania76@gmail.com', ' devi456', ' v.jpg'),
+(' Dewi Nurhasanah', '6701173344', 's1ab41', ' makan', 'Horror', 'Tanjung Selor', '2018-10-22', 'dewinurhasanah', 'dewinurhasanah@gmail.com', 'dewi123', 'icon.png');
 
 --
 -- Indexes for dumped tables
